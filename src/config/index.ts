7c18348ba9ad081ff2100f5215fa9117157d1950
melvin <melvin.mark@centrino.co.ke>
2022-10-05 16:40:55 +0300

    build & fix: modify mongo db connection string

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,4 @@
 // config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE,DB_PASSWORD, SECRET_KEY, DB_USER,LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE,DB_CONNECTION_STRING,DB_PASSWORD, SECRET_KEY, DB_USER,LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,3 +1,3 @@
-import { DB_HOST, DB_PORT, DB_DATABASE,DB_USER,DB_PASSWORD } from '@config';
+import { DB_HOST, DB_PORT, DB_DATABASE,DB_USER,DB_PASSWORD, DB_CONNECTION_STRING } from '@config';
 
-export const dbConnection = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+export const dbConnection = DB_CONNECTION_STRING;
